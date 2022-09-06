6bc8a227499ee0e664989665c6339479e6871fea
Victor-Diaz15 <Victordiaz123julioarias@gmail.com>
2022-09-05 22:33:40 -0400

    Add changes in appsettings and startup

--- StudentManagement.WebApi/Startup.cs
+++ StudentManagement.WebApi/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using StudentManagement.Core.Application;
+using StudentManagement.Infrastructure.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,8 @@ namespace StudentManagement.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddPersistanceInfrastructure(Configuration);
+            services.AddApplicationLayer();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
